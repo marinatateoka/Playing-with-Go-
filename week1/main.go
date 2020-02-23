570558f5838083f0cc8126059a0aefed6c636b7e
Marina Tateoka <marinaslussarek@gmail.com>
2020-02-23 20:36:23 +1300

    add function to remove spaces

--- week1/main.go
+++ week1/main.go
@@ -2,11 +2,35 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
 func main() {
-	res1 := strings.SplitN("1, 2, 3, 4, 5, 6", ",", 6)
+	l, err := ReturnIntList("1, 2, 3,4, 5, 6")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(l)
 
-	fmt.Printf("\nSlice 1: %s", res1)
+	l, err = ReturnIntList("1, 2, 3,4, 5, 6")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(l)
+}
+
+func ReturnIntList(myStr string) ([]int, error) {
+	newStr := strings.Replace(myStr, " ", "", -1)
+	res1 := strings.Split(newStr, ",")
+	res2 := []int{}
+
+	for _, i := range res1 {
+		j, err := strconv.Atoi(i)
+		if err != nil {
+			return nil, (err)
+		}
+		res2 = append(res2, j)
+	}
+	return res2, nil
 }
